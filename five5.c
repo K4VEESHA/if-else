f410dd13d153e418bbd4b1bdf85db52bedc2d895
Kaveesha Thilakarathna <72226897+K4VEESHA@users.noreply.github.com>
2020-10-02 10:15:49 +0530

    Add files via upload

---
+++ five5.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main ()
+
+{
+int odd=0,even=0,x=1;
+for(int i=1;i<=10;i++)
+
+{
+printf("Enter Number : ");
+scanf("%d",&x);
+
+if(x%2==0)
+    even++;
+else
+    odd++;
+
+
+}
+
+printf("Total Number of even is %d \n",even);
+printf("Total Number of odd is %d \n",odd);
+
+}
